0f67f89d1224d8277199930bb3d3ce53b99042ef
Jennifer <jennny.gc@gmail.com>
2015-10-21 22:35:57 +0200

    Created singleton.factory

--- src/main/java/singleton/factory/ReferencesFactory.java
+++ src/main/java/singleton/factory/ReferencesFactory.java
@@ -4,27 +4,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReferencesFactory {
-    private Map<String, Integer> references;
 
-    private int reference;
+	private static final ReferencesFactory factory = new ReferencesFactory();
+	private Map<String, Integer> references;
+	private int reference;
 
-    public ReferencesFactory() {
-        this.references = new HashMap<>();
-        this.reference = 0;
-    }
+	public ReferencesFactory() {
+		this.references = new HashMap<String, Integer>();
+		this.reference = 0;
+	}
 
-    public int getReference(String key) {
-        Integer result = this.references.get(key);
-        if (result == null) {
-            this.references.put(key, this.reference);
-            result = this.reference;
-            reference++;
-        }
-        return result;
-    }
+	public static ReferencesFactory getFactory() {
+		return factory;
+	}
 
-    public void removeReference(String key) {
-        this.references.remove(key);
-    }
+	public int getReference(String key) {
+		Integer result = this.references.get(key);
+		if (result == null) {
+			this.references.put(key, this.reference);
+			result = this.reference;
+			reference++;
+		}
+		return result;
+	}
+
+	public void removeReference(String key) {
+		this.references.remove(key);
+	}
 
 }
